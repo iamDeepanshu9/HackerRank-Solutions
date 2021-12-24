d3f45ced90c97a2b4c18c946df371996601d19ee
iamdeepanshu9 <iamdeepanshu9@gmail.com>
2021-12-24 21:36:11 +0530

    Where is the Digit in the String

--- src/com/Study/HackerRank/WhereTheDigit.java
+++ src/com/Study/HackerRank/WhereTheDigit.java
@@ -1,5 +1,7 @@
 package com.Study.HackerRank;
 
+import java.util.Scanner;
+
 public class WhereTheDigit {
 //    The input string should not contain only integers or only alphabets.
 //    The number of integers in the input string should not be less than 4.
@@ -7,16 +9,18 @@ public class WhereTheDigit {
 //    Explanation: There are 6 integers in the string which are at the index 3,4,6,7,10,11.
 //    These indexes are joined to form a single numeric string 34671011.
     public static void main(String[] args) {
-        String str = "ABC65D19HY09";
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter only Alphanumeric String : ");
+        String str = sc.next();
         System.out.println(indexString(str));
     }
     public static String indexString(String s) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
-            if(Character.isDigit(s.charAt(i))) result += i ;
+            if(Character.isDigit(s.charAt(i))) result.append(i);
 
         }
-        return result;
+        return result.toString();
     }
 
 }
